e93c171b1bf6a304a252372592d6df9bb72fb08d
uf0 <scnepa222@gmail.com>
2017-02-09 18:15:14 +0100

    updated file saving for multilanguage

--- routes/calendar.js
+++ routes/calendar.js
@@ -2,6 +2,7 @@ var express = require('express');
 var request = require('request');
 var config = require('../config/config.js');
 var fs = require('fs');
+var async = require('async');
 var router = express.Router();
 
 
@@ -20,12 +21,63 @@ router.get('/', function(req, res) {
     function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-      fs.writeFile(config.api.calendar.path + 'calendar.json', body, function(err) {
-          if(err) {
+      var json = JSON.parse(body);
+
+      json = json.items;
+
+      var json_fr = [],
+          json_en = [];
+
+      json.forEach(function(e){
+
+        var elm_fr = {
+          htmlLink: e.htmlLink,
+          location: e.location,
+          start: e.start.dateTime,
+          end: e.end.dateTime,
+          hangoutLink: e.hangoutLink,
+          summary: e.summary.split('|')[0],
+          description: e.description.split('|')[0]
+        }
+
+        json_fr.push(elm_fr);
+
+        var summary_en = e.summary.split('|').length>1?e.summary.split('|')[1]:e.summary.split('|')[0];
+        var description_en = e.description.split('|').length>1?e.description.split('|')[1]:e.description.split('|')[0];
+
+        var elm_en = {
+          htmlLink: e.htmlLink,
+          location: e.location,
+          start: e.start.dateTime,
+          end: e.end.dateTime,
+          hangoutLink: e.hangoutLink,
+          summary: summary_en,
+          description: description_en
+        }
+
+        json_en.push(elm_en);
+
+      })
+
+      var files = [
+        {lang:'fr', content: json_fr},
+        {lang:'en', content: json_en}
+      ]
+
+      async.each(files, function (file, callback) {
+
+          fs.writeFile(config.api.calendar.path + 'calendar_' + file.lang +'.json', JSON.stringify(file.content, null, 4), function (err) {
+              callback();
+          });
+
+      }, function (err) {
+
+          if (err) {
               res.send({status:'error', message:err});
-              return;
           }
-          res.send({status:'ok', message:'The file was saved!'})
+          else {
+              res.send({status:'ok', message:'The files were saved!'})
+          }
       });
 
     }else{
--- routes/resources.js
+++ routes/resources.js
@@ -2,6 +2,8 @@ var express = require('express');
 var request = require('request');
 var config = require('../config/config.js');
 var fs = require('fs');
+var d3 = require('d3-dsv');
+var async = require('async');
 var router = express.Router();
 
 
@@ -21,11 +23,78 @@ router.get('/', function(req, res) {
     function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-      fs.writeFile(config.api.projects.path + 'resources.tsv', body, function(err) {
-          if(err) {
-              return res.send({status:'error', message:err});
+      var json = d3.tsvParse(body);
+
+      var translatedFields = {
+        'en' : [
+          'name_en',
+          'category_en',
+          'presentation_en'
+        ],
+        'fr': [
+          'name_fr',
+          'category_fr',
+          'presentation_fr'
+        ]
+      }
+
+      var json_fr = json.map(function(d){
+
+        var elm = {};
+        for(field in d){
+          elm[field] = d[field]
+        }
+
+        translatedFields.en.forEach(function(f){
+          delete elm[f]
+        })
+
+        translatedFields.fr.forEach(function(f){
+          elm[f.replace('_fr','')] = elm[f]
+          delete elm[f]
+        })
+
+        return elm
+      })
+
+      var json_en = json.map(function(d){
+
+        var elm = {};
+        for(field in d){
+          elm[field] = d[field]
+        }
+
+        translatedFields.fr.forEach(function(f){
+          delete elm[f]
+        })
+
+        translatedFields.en.forEach(function(f){
+          elm[f.replace('_en','')] = elm[f]
+          delete elm[f]
+        })
+
+        return elm
+      })
+
+      var files = [
+        {lang:'fr', content: json_fr},
+        {lang:'en', content: json_en}
+      ]
+
+      async.each(files, function (file, callback) {
+
+          fs.writeFile(config.api.resources.path + 'resources_' + file.lang +'.json', JSON.stringify(file.content, null, 4), function (err) {
+              callback();
+          });
+
+      }, function (err) {
+
+          if (err) {
+              res.send({status:'error', message:err});
           }
-          res.send({status:'ok', message:'The file was saved!'})
+          else {
+              res.send({status:'ok', message:'The files were saved!'})
+          }
       });
 
     }else{
