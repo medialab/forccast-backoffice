a00b17ba76bde8e48743f54a71dafff10ff4fdd4
Robin de Mourat <robin.demourat@gmail.com>
2019-09-20 15:59:12 +0200

    fix small issues for events

--- routes/calendar.js
+++ routes/calendar.js
@@ -5,12 +5,21 @@ var fs = require('fs');
 var async = require('async');
 var router = express.Router();
 
+var normalizeDigits = function(num) {
+  var str = '' + num;
+  return str.length > 1 ? str : '0' + str;
+}
+
+var formatContent = function(html) {
+  return html
+          .replace(/\n/g, '<br />')
+}
+
 
 /* GET events listing. */
 router.get('/', function(req, res) {
 
   var url = 'https://www.googleapis.com/calendar/v3/calendars/' + config.api.calendar.calendarId + '/events';
-
   var params = {
     key: config.api.calendar.apiKey,
     timeMin: new Date()
@@ -36,12 +45,17 @@ router.get('/', function(req, res) {
         var end = hasDateTime ? e.end.dateTime : e.end.date;
         const HOUR = 3600 * 1000;
         var DAY = HOUR * 24;
-        if (start && end && new Date(end).getHours() === 1) {
+        // if time of end is 00:00 --> -1day
+        if (start && end && new Date(end).getHours() === 2) {
           var diff = Math.abs(new Date(end).getTime() - new Date(start).getTime());
           if (diff%DAY === 0) {
-            end = new Date(new Date(end).getTime() - HOUR * 2);
+            end = new Date(end)
+            end = end.getFullYear() + '-' + normalizeDigits(end.getMonth() + 1) + '-' + (end.getDate() - 1);
           }
         }
+
+        var summary_fr = formatContent(e.summary && e.summary.length ? e.summary.split('|')[0] : e.summary);
+        var description_fr = formatContent(e.description && e.description.length ? e.description.split('|')[0] : e.description);
 
         var elm_fr = {
           htmlLink: e.htmlLink,
@@ -50,14 +64,14 @@ router.get('/', function(req, res) {
           end,
           hasDateTime: hasDateTime,
           hangoutLink: e.hangoutLink,
-          summary: e.summary && e.summary.length ? e.summary.split('|')[0] : e.summary,
-          description: e.description && e.description.length ? e.description.split('|')[0] : e.description
+          summary: summary_fr,
+          description: description_fr
         }
 
         json_fr.push(elm_fr);
 
-        var summary_en = e.summary && e.summary.length ? e.summary.split('|').length>1?e.summary.split('|')[1]:e.summary.split('|')[0] : e.summary;
-        var description_en = e.description && e.description.length ?  e.description.split('|').length>1?e.description.split('|')[1]:e.description.split('|')[0] : e.description;
+        var summary_en = formatContent(e.summary && e.summary.length ? e.summary.split('|').length>1?e.summary.split('|')[1]:e.summary.split('|')[0] : e.summary);
+        var description_en = formatContent(e.description && e.description.length ?  e.description.split('|').length>1?e.description.split('|')[1]:e.description.split('|')[0] : e.description);
 
 
 
